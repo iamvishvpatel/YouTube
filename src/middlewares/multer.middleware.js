d711d15e7ca060c50d35b2971bbd6c5961b7f852
iamvishvpatel <vishvvadol95@gmail.com>
2025-03-22 19:06:22 +0530

    add multer and cloudinary files for file upload

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,16 @@
+import multer from "multer";
+import path from 'path'
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+        const ext = path.extname(file.originalname); // Get file extension
+        cb(null, file.fieldname + '-' + uniqueSuffix + ext);
+    }
+  })
+  
+export const upload = multer({ 
+    storage, 
+})
