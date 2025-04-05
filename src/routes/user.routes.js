54017171592c5103fade71641d222d2d5133349c
iamvishvpatel <vishvvadol95@gmail.com>
2025-04-06 03:18:26 +0530

    Added More secure Route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express'
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from '../controllers/user.controllers.js'
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updatedUserAvatar, updatedUserCoverImage } from '../controllers/user.controllers.js'
 import {upload} from '../middlewares/multer.middleware.js'
 import { verifyJWt } from '../middlewares/auth.middleware.js'
 
@@ -26,5 +26,11 @@ router.route("/login").post(loginUser)
 //secure route
 router.route("/logout").post(verifyJWt ,logoutUser)
 router.route("/refreshToken").post(refreshAccessToken)
-
+router.route("/change-password").post(verifyJWt, changeCurrentPassword)
+router.route("/current-user").get(verifyJWt, getCurrentUser)
+router.route("/update-account").patch(verifyJWt, updateAccountDetails)
+router.route("/avatar").patch(verifyJWt, upload.single("avatar"), updatedUserAvatar)
+router.route("/coverImage").patch(verifyJWt, upload.single("coverImage", updatedUserCoverImage))
+router.route("/c/:username").get(verifyJWt, getUserChannelProfile)
+router.route("/history").get(verifyJWt, getWatchHistory)
 export default router
